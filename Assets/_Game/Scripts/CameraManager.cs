73f9964fdd347887c8e2b3e7bc3d30be27cd1041
Luiz Henrique Harsche <henrique_harsche@hotmail.com>
2021-10-02 04:02:52 -0300

    Prefabs de props externos e início da montagem da vila

--- Assets/_Game/Scripts/CameraManager.cs
+++ Assets/_Game/Scripts/CameraManager.cs
@@ -4,6 +4,19 @@ using Cinemachine;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    public static GameObject PlayerCamera;
+
+    private void Start()
+    {
+        if(PlayerCamera == null)
+        {
+            PlayerCamera = gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void CameraFollowPlayer()
     {
