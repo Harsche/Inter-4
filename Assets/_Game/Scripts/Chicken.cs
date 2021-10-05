ab0631ac41794ccce895668b9c854c235ef8e222
Luiz Henrique Harsche <henrique_harsche@hotmail.com>
2021-10-05 10:24:33 -0300

    Galinha adicionada

--- Assets/_Game/Scripts/Chicken.cs
+++ Assets/_Game/Scripts/Chicken.cs
@@ -3,9 +3,12 @@ using DG.Tweening;
 
 public class Chicken : MonoBehaviour
 {
+    [SerializeField] float runDistance;
+    [SerializeField] float runTime;
     private Animator myAnimator;
     private Rigidbody2D myRigibody2d;
     private SpriteRenderer mySpriteRenderer;
+    private Tween run;
 
     void Start()
     {
@@ -14,14 +17,28 @@ public class Chicken : MonoBehaviour
         mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void FixedUpdate()
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        mySpriteRenderer.flipX = myRigibody2d.velocity.x < 0 ? true : false;
-        myAnimator.SetFloat("Velocity", myRigibody2d.velocity.magnitude);
+        run.Kill();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.CompareTag("Player"))
+        {
+            Vector2 myPosition = transform.position;
+            Vector2 distance = transform.position - other.transform.position;
+            mySpriteRenderer.flipX = distance.x < 0 ? true : false;
+            distance = myPosition + Vector2.ClampMagnitude(distance, 1.0f) * runDistance;
+            run = myRigibody2d.DOMove(distance, runTime);
 
+            myAnimator.SetFloat("Velocity", 1.0f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        myAnimator.SetFloat("Velocity", 0.0f);
     }
 }
