6232525e536934152e0329c97b3ce330ae732bfd
Luiz Henrique Harsche <henrique_harsche@hotmail.com>
2021-11-20 19:17:58 -0300

    Arrumei o botão de confirmar novo jogo

--- Assets/_Game/Scripts/Setup/StartScreen.cs
+++ Assets/_Game/Scripts/Setup/StartScreen.cs
@@ -1,23 +1,61 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class StartScreen : MonoBehaviour
 {
+    [SerializeField] private GameObject confirmNewGame;
+    [SerializeField] private GameObject[] objectsToTurnOff;
+    [SerializeField] private CutsceneSOData cutsceneSOData;
+    [SerializeField] private QuestList questList;
+    [SerializeField] private CharactersSOData charactersSOData;
+
+    private bool hasSave;
+
     private void Awake()
     {
+        hasSave = CheckIfThereIsSaveFile();
         Application.targetFrameRate = 60;
     }
 
     public void NewGameButton()
     {
-        SaveManager.DeleteSave();
-        SaveManager.NewSaveFile();
+        if (hasSave)
+        {
+            ActivateOrDeactivate(false);
+            confirmNewGame.SetActive(true);
+            return;
+        }
+        StartNewGame();
     }
 
     public void LoadGameButton()
     {
         SaveManager.LoadGame();
-        if(SaveManager.saveFile != null)
+        if (SaveManager.saveFile != null)
             SceneManager.LoadScene("Load");
+    }
+
+    private bool CheckIfThereIsSaveFile()
+    {
+        string path = Application.persistentDataPath + @"\gamedata.json";
+        return File.Exists(path);
+    }
+
+    public void StartNewGame()
+    {
+        SaveManager.DeleteSave();
+        SaveManager.NewSaveFile();
+        cutsceneSOData.SetAllPlayable();
+        questList.ResetQuests();
+        charactersSOData.ResetCharacters();
+         if (SaveManager.saveFile != null)
+            SceneManager.LoadScene("Load");
+    }
+
+    public void ActivateOrDeactivate(bool active)
+    {
+        foreach(GameObject obj in objectsToTurnOff)
+            obj.SetActive(active);
     }
 }
