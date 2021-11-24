7cb14e3f19be5bb75c91b778c0454120f67664ff
Luiz Henrique Harsche <henrique_harsche@hotmail.com>
2021-11-23 22:19:10 -0300

    Commit teste

--- Assets/_Game/Scripts/Tasks/Pick Water/WaterWell.cs
+++ Assets/_Game/Scripts/Tasks/Pick Water/WaterWell.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class WaterWell : MonoBehaviour
 {
-    
+    private const string bucketParameter = "Bucket";
+    private Animator playerAnimator;
+
+    private void Awake() {
+        playerAnimator = Globals.Player.GetComponent<Animator>();
+    }
+
+    public void GetWater()
+    {
+        bool withBucket = playerAnimator.GetBool(bucketParameter);
+    }
 }
